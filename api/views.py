4c63f5c657dd9517008082b90a8d7c38cd2ce717
Vincent Joy <vincentjoyhere@gmail.com>
2025-04-16 17:21:36 +0530

    fixed the exception

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import Movie
 from .serializers import MovieSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
