ba374cc981d40429a80968adfc3912f0b78ec4a5
Vincent Joy <vincentjoyhere@gmail.com>
2025-04-16 20:03:26 +0530

    moved the login url to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import RegisterUserView, UserDetailView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('me/', UserDetailView.as_view(), name='user-detail'),
+    path('login/', obtain_auth_token, name='login'),
 ]
