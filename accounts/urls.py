f91e2169a653d6f88e58700c23307249a7ff3f7a
Vincent Joy <vincentjoyhere@gmail.com>
2025-04-18 14:10:14 +0530

    added the retrieve and update use case for User model, in order to dispaly the user details and edit feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('me/', UserDetailView.as_view(), name='user-detail'),
     path('login/', LoginView.as_view(), name='login'),
 ]
